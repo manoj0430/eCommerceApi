d54391d72153bca891ac29ccffc71c81b44e242a
manasa lingabathula <manojl0430@gmail.com>
2024-02-19 13:07:18 +0530

    Add devEnv

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ const db = mongoose.connection;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/eCommerceApi');
+  await mongoose.connect(process.env.MONGO_URL);
 
   console.log("My db is working");
   
